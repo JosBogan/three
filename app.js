f305e739b4702b1dbc95224d9bca516fdf1383be
Jos Bogan <Josbogan@gmail.com>
2020-07-15 18:08:44 +0100

    testing 3d model loading optinos

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ function init() {
 
     scene = new THREE.Scene()
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    // console.log(THREE)
   
     renderer = new THREE.WebGLRenderer( { antialias: true } )
 
 
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
+
+    // Creating Box Geom
 
     const geometry = new THREE.BoxGeometry(2, 2, 2)
     const material = new THREE.MeshBasicMaterial( { color: 0xff00ff } )
@@ -55,8 +59,10 @@ function init() {
       const diff = [newCoords[0] - clicked.coords[0], newCoords[1] - clicked.coords[1]]
       // const diff = [clicked.coords[0] - newCoords[0], clicked.coords[1] - newCoords[1]]
 
-      cube.rotation.x += diff[1] / 10000
-      cube.rotation.y += diff[0] / 10000
+      cube.rotation.x += diff[1] / 300
+      cube.rotation.y += diff[0] / 300
+
+      clicked.coords = newCoords
 
 
     }
